4fb8c6000dd1d0b2f853a4d1f59372e82a465713
Arthur Bourgoin <arthur_bourgoin@orange.fr>
2023-12-16 23:11:21 +0100

    start classes

---
+++ app/class/Training.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Class;
+
+class Training {
+
+    const PATH_IMG = "/assets/images/Formations/";
+    public $idTraining;
+    public $wording;
+    public $description;
+    public $qualifLevel;
+    public $picture;
+    public $picturePath;
+
+    public function __construct(object $obj) {
+        $this->idTraining=$obj->idTraining;
+        $this->wording=$obj->wording;
+        $this->description=$obj->description;
+        $this->qualifLevel=$obj->qualifLevel;
+        $this->picture=$obj->picture;
+        $this->picturePath = self::PATH_IMG . $this->picture;
+    }
+
+    public function getBsCard() {
+
+    }
+
+    // $footer = "connexion" || "sadmin" || "admin"
+    public function getDenomination($footer): String {
+
+    }
+
+    public function getCardChooseTraining() {
+        ob_start();?>
+        <form action="<?= $_SERVER["REQUEST_URI"] ?>" method="POST" class="row mb-3 col-12 col-md-5">
+            <input type="hidden" name="idTraining" value="<?= $this->idTraining ?>">
+            <div class="divTraining d-flex align-items-center border border-dark rounded-3" role="button">
+                <div class="col-10 col-md-4">
+                    <div class="text-center">
+                        <img src= <?= $this->picturePath ?> class="w-75 my-4">
+                    </div>
+                </div>
+                <div class="col-8">
+                    <h3 class="fw-bold text-center lh-base"><?= htmlentities($this->wording)?></h3>
+                </div>
+            </div>
+        </form>
+        <?php
+        return ob_get_clean();
+       
+    }
+
+}
--- app/class/User.php
+++ app/class/User.php
@@ -10,19 +10,52 @@ class User {
     public $firstName;
     public $picture;
     public $typePwd;
-    public $pwd;
     public $role;
-    public $idtraing;
+    public $idTraining;
 
-    public function __construct(int $id,string $login,string $lastName,string $firstName,string $picture,string $typePwd,string $pwd,int $idtraing) {
-        $this->id = $id;
-        $this->login = $login;
-        $this->lastName = $lastName;
-        $this->firstName = $firstName;
-        $this->picture = $picture;
-        $this->typePwd = $typePwd;
-        $this->pwd = $pwd;
-        $this->idtraing = $idtraing;
+    public $comments;
+
+
+    public function __construct(object $obj, array $comments) {
+        $this->idUser=$obj->idUser;
+        $this->login=$obj->login;
+        $this->lastName=$obj->lastName;
+        $this->firstName=$obj->firstName;
+        $this->picture=$obj->picture;
+        $this->typePwd=$obj->typePwd;
+        $this->role=$obj->role;
+        $this->idTraining=$obj->idTraining;
+        $this->comments = $comments;
+    }
+
+    public function getBsCard() {
+
+    }
+
+    // $footer = "connexion" || "sadmin" || "admin"
+    public function getDenomination($footer): String {
+
+    }
+
+    public function getCardConnexion() {
+        switch($this->typePwd){
+            case 1:
+                $target = "modalConnexionTexte";break;
+            case 2:
+                $target = "modalConnexionCode";break;
+            case 3:
+                $target = "modalConnexionSchema";break;
+        }
+        ob_start();?>
+        <div class="card mb-4 mx-2 col-lg-3 col-md-4 col-6 border-dark" data-id="<?= $this->idUser ?>" data-bs-toggle="modal" data-bs-target="#<?=$target?>">
+            <img src="<?= htmlentities($this->picture)?>" class="img-thumbnail border-0 mt-4" alt="Photo de l'étudiant <?= htmlentities($this->idUser)?>">
+            <div class="card-body text-center">'
+                <h5 class="card-title"><?=htmlentities($this->lastName)?></h5>
+                <p class="card-text"><?= htmlentities($this->firstName)?></p>
+            </div>
+        </div>
+        <?php
+        return ob_get_clean();
     }
 
 
